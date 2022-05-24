26340100d6e270b36e668c0745df617f495a30c0
OthmanAdi <github_me@outlook.de>
2022-05-24 15:44:52 +0200

    version tuseday 24.05.22

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function AddTask(){
+function AddTask() {
     var eingabeFeld = document.getElementById("myInput").value;
     var textNode = document.createTextNode(eingabeFeld);
     var makeNewItem = document.createElement("li");
@@ -7,3 +7,20 @@ function AddTask(){
 
     document.getElementById("myUL").appendChild(makeNewItem);
 }
+
+const gradientGenerator = () => {
+    let rgbaMagic = "rgba(";
+    for (let start = 0; start < 3; start++) {
+        rgbaMagic += Math.floor(Math.random() * 255) + ",";
+    }
+    return rgbaMagic
+}
+
+const lightsOn = () => {
+    let toFirstVariable = gradientGenerator();
+    let toSecondVariable = gradientGenerator();
+    document.documentElement.style.setProperty("--GradientColor1" , toFirstVariable + "1)");
+    document.documentElement.style.setProperty("--GradientColor2" , toSecondVariable + "1)");
+}
+
+setInterval(lightsOn,1000);
